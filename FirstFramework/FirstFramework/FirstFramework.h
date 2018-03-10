1271701a7d074d926503369fa1fac11ae0f00ad5
Imanuel Pardosi <ipardosi@dyned.com>
2018-03-10 22:29:35 +0700

    Finished our first framework

---
+++ FirstFramework/FirstFramework/FirstFramework.h
@@ -0,0 +1,19 @@
+//
+//  FirstFramework.h
+//  FirstFramework
+//
+//  Created by Mobile Jakarta Team on 10/03/18.
+//  Copyright © 2018 moonshadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirstFramework.
+FOUNDATION_EXPORT double FirstFrameworkVersionNumber;
+
+//! Project version string for FirstFramework.
+FOUNDATION_EXPORT const unsigned char FirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFramework/PublicHeader.h>
+
+
---
+++ FirstFramework/FirstFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  FirstFramework
+//
+//  Created by Mobile Jakarta Team on 10/03/18.
+//  Copyright © 2018 moonshadow. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {
+        
+    }
+    
+    public static func doSomething() -> String {
+        return "Did some stuff, here you go dude."
+    }
+}
